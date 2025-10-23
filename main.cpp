a08f41b9b1a87b15348129f51e3b5f781223fa61
s1140324-a11y <s1140324@dmhs.kh.edu.tw>
2025-10-23 09:45:20 +0800

    hw3
    hw3

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
-#include <string>
 
-int main() {
-    int age = 99;
-    double height = 200;
-    std::string name = "Terry";
-    bool is_pass = true;
-    std::cout << "姓名: " << name << "身高: " << height << std::endl;
-    std::cout << "年齡: " << age << "是否及格: " << is_pass << std::endl;
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "1. 快樂星球" << endl;
+    cout << "2. 快樂 星球" << endl;
+    cout << "3. 快樂\t星球" << endl;
+    cout << "4. 快樂\n星球" << endl;
+    cout << "5. \"快樂星球\"" << endl;
+    cout << "6.\' 快樂星球\'" << endl;
+    cout << "7. \\快樂星球\\" << endl;
+    
     return 0;
 }
